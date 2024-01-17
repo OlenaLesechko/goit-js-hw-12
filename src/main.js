4c3caacd5f61e13b44760f5fa224efc25b95ba5b
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2024-01-17 22:02:16 +0100

    library

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 "use strict"
 
 import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
